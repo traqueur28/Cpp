e5c4133cd415ad362fd7cbd08182594fbe9c7d45
Jarod Gourlin <jgourlin@MacBook-Air-de-Jarod.local>
2022-07-15 10:47:52 +0200

    day-06 ex01

---
+++ Module_06/ex01/class/serialize.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serialize.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgourlin <jgourlin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 10:19:40 by jgourlin          #+#    #+#             */
+/*   Updated: 2022/07/15 10:46:53 by jgourlin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZE_HPP
+# define SERIALIZE_HPP
+
+# include <iostream>
+# include <cstdint>
+
+typedef struct Data
+{
+	int	A;
+	int	B;
+}			Data;
+
+
+#endif
